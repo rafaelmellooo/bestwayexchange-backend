f839b5f661a83ef660cd505bf681694ac9eaddfd
Rafael Mello <rafaelmello0715@outlook.com>
2019-10-02 15:30:45 -0300

    Added agency controller

---
+++ src/app/controllers/AgencyController.js
@@ -0,0 +1,51 @@
+const { Agency } = require('../models')
+
+module.exports = {
+  async show (req, res) {
+    try {
+      const agency = Agency.findByPk(req.params.id)
+
+      return res.status(200).json(agency)
+    } catch (err) {
+      return res.status(400).json(err)
+    }
+  },
+
+  async store (req, res) {
+    try {
+      await Agency.create(req.body)
+
+      res.status(200).json()
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  },
+
+  async update (req, res) {
+    try {
+      await Agency.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      })
+
+      res.status(200).json()
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  },
+
+  async destroy (req, res) {
+    try {
+      await Agency.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+
+      res.status(200).json()
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  }
+}
