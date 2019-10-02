5abb129313c3a6ea75540c6607baa93e115b25a2
Rafael Mello <rafaelmello0715@outlook.com>
2019-10-02 02:32:43 -0300

    Fixes in favorite table test mass

--- src/database/seeders/20190930043016-demo-favorite.js
+++ src/database/seeders/20190930043016-demo-favorite.js
@@ -3,11 +3,26 @@
 module.exports = {
   up: queryInterface => {
     const data = []
+    const favorites = []
 
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 60; i++) {
+      let exchangeId = Math.floor((Math.random() * 30) + 1)
+      let userId = Math.floor((Math.random() * 20) + 1)
+
+      while (
+        favorites.some(favorite => favorite[0] === exchangeId && favorite[1] === userId)
+      ) {
+        exchangeId = Math.floor((Math.random() * 30) + 1)
+        userId = Math.floor((Math.random() * 20) + 1)
+      }
+
+      favorites.push([
+        exchangeId, userId
+      ])
+
       data.push({
-        exchangeId: Math.floor((Math.random() * 30) + 1),
-        userId: Math.floor((Math.random() * 20) + 1),
+        exchangeId,
+        userId,
         createdAt: new Date()
       })
     }
